84e22e1b318b5fb2cbc151e138ca39a1bcc196cf
Poleszak <poleszak@protonmail.com>
2022-01-06 18:42:07 +0100

    Initial commit

---
+++ src/test/java/com/poleszak/CoronaVirusTracker/CoronaVirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poleszak.CoronaVirusTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaVirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
