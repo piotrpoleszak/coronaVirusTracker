90aac3a1b77863e8d7847f3551b929cd2a9f2672
Poleszak <poleszak@protonmail.com>
2022-01-11 18:18:27 +0100

    Show stats and total reported cases

--- src/main/java/com/poleszak/CoronaVirusTracker/controllers/HomeController.java
+++ src/main/java/com/poleszak/CoronaVirusTracker/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package com.poleszak.CoronaVirusTracker.controllers;
 
+import com.poleszak.CoronaVirusTracker.models.LocationStats;
 import com.poleszak.CoronaVirusTracker.services.CoronaVirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController
@@ -15,7 +18,12 @@ public class HomeController
     @GetMapping("/")
     public String home(Model model)
     {
-        model.addAttribute("locationStats", coronaVirusDataService.getAllStats());
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+
         return "home";
     }
 }
